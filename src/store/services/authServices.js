2c1b5f3759f165e9cdac17be12a7038aa1704b6b
rasadov <raufasadov23@gmail.com>
2025-01-16 15:55:23 +0400

    fixes

--- src/store/services/authServices.js
+++ src/store/services/authServices.js
@@ -45,9 +45,10 @@ export const registerUser = createAsyncThunk(
     });
     const data = await response.json();
     localStorage.setItem("user", data);
+    window.location.href = "/";
     if (response.ok) {
-      dispatch(setUser(data));
-      return data;
+      // dispatch(setUser(data));
+      // return data;
     } else {
       throw new Error(data.message || "Registration failed");
     }
