fcaccfa5240ddd6b5f2ecbd105223f678568d850
rasadov <raufasadov23@gmail.com>
2025-01-17 12:31:09 +0400

    fixes

--- src/pages/Complex.jsx
+++ src/pages/Complex.jsx
@@ -36,98 +36,75 @@ const Complex = () => {
   if (!listing) return <div>No complex details available</div>;
   console.log("PROPERTY DETAILS", properties);
   return (
-    <div className="flex flex-col min-h-screen ">
-      <main className="flex-grow bg-[#F4F2FF] ">
-        <Header />
-        {properties ? (
-
-        <div className="px-16 mx-auto max-w-[1440px] bg-white">
-          <div className="w-full py-3 mx-auto bg-white">
-            <div className="flex flex-col ">
-              <div className="w-full">
-
-                  <Breadcrumbs title={listing.id} />
-                <PropertyShowcase length={listing.images.length} images={listing.images} />
-                {/* grid area */}
-                <div className="grid grid-cols-1 gap-6 my-12 md:grid-cols-4 lg:grid-cols-6">
-                  {Object.entries(properties)
-                    .filter(([key]) =>
-                      [
-                        "category",
-                        "buildingArea",
-                        "livingArea",
-                        "objects",
-                        "installment",
-                        "completionYear",
-                      ].includes(key)
-                    )
-                    .map(([key, value], index) => (
-                      <div
-                        key={index}
-                        className="flex justify-start w-full min-h-[50px] gap-2"
-                      >
-                        <div className="rounded-full bg-[#CACDD5] w-[50px] h-[50px]"></div>
-                        <div>
-                          <span className="text-[#525C76] font-medium text-[14px] leading-[22.4px] capitalize">
-                            {key.replace(/([A-Z])/g, " $1").trim()}
-                          </span>
-                          <p className="text-[#0F1D40] text-[14px] leading-[22.4px] font-medium">
-                            {value}
-                          </p>
-                        </div>
-                      </div>
-                    ))}
-                </div>
-                {/* description */}
-                <div className="my-14">
-                  <h1 className="text-[#0F1D40] font-semibold text-[36px] leading-[54px]">
-                    Description
-                  </h1>
-                  <p className="text-[#0F1D40] font-normal text-[18px] leading-[28.8px]">
-                    {listing.description}
-                  </p>
-                  {/* <span>{properties.updated}</span> */}
-                  <hr className="my-8 border-t-2 border-[#EEEFF2]" />
-                </div>
-
-              </div>
-            </div>
-            {/* <div className="w-[96%] mx-auto my-20">
-              {popularProperties ? (
-                <div className="grid grid-cols-1 gap-6 mx-auto sm:grid-cols-2 lg:grid-cols-4">
-                  {popularProperties.map((item) => (
-                    <HouseItem key={item.id} {...item} />
-                  ))}
-                </div>
-              ) : (
-                <p className="text-center">
-                  No related properties available yet.
-                </p>
-              )}
-              <Pagination />
-            </div> */}
-            {/* location */}
-            <div className="w-full border-t-2 py-14">
-              <h2 className="text-[#0F1D40] font-semibold text-[36px] leading-[54px]">
-                Location
-              </h2>
-              <PropertyMap
-              location={{
-                "latitude": listing.latitude,
-                "longitude": listing.longitude,
-              }}
-                />
-            </div>
+    <div className="px-16 mx-auto max-w-[1440px] bg-white">
+      <Breadcrumbs title={listing.name} />
+      <PropertyShowcase length={listing.images.length} images={listing.images} />
+  
+      {/* Grid Area */}
+      <div className="grid grid-cols-1 gap-6 my-12 md:grid-cols-4 lg:grid-cols-6">
+        <div className="flex items-center gap-4">
+          <div className="rounded-full bg-[#CACDD5] w-[50px] h-[50px]"></div>
+          <div>
+            <span className="text-[#525C76] font-medium text-[14px] leading-[22.4px] capitalize">Category</span>
+            <p className="text-[#0F1D40] text-[14px] leading-[22.4px] font-medium">{listing.category || "N/A"}</p>
           </div>
         </div>
-        ) : ("No complex details available")}
-
-        <div className="px-6 mx-auto bg-[#ECE8FF]">
-          <Footer />
+        <div className="flex items-center gap-4">
+          <div className="rounded-full bg-[#CACDD5] w-[50px] h-[50px]"></div>
+          <div>
+            <span className="text-[#525C76] font-medium text-[14px] leading-[22.4px] capitalize">Building Area</span>
+            <p className="text-[#0F1D40] text-[14px] leading-[22.4px] font-medium">{listing.building_area || "0.0"} m²</p>
+          </div>
         </div>
-      </main>
+        <div className="flex items-center gap-4">
+          <div className="rounded-full bg-[#CACDD5] w-[50px] h-[50px]"></div>
+          <div>
+            <span className="text-[#525C76] font-medium text-[14px] leading-[22.4px] capitalize">Living Area</span>
+            <p className="text-[#0F1D40] text-[14px] leading-[22.4px] font-medium">{listing.living_area || "0.0"} m²</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="rounded-full bg-[#CACDD5] w-[50px] h-[50px]"></div>
+          <div>
+            <span className="text-[#525C76] font-medium text-[14px] leading-[22.4px] capitalize">Objects</span>
+            <p className="text-[#0F1D40] text-[14px] leading-[22.4px] font-medium">{listing.objects || "N/A"}</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="rounded-full bg-[#CACDD5] w-[50px] h-[50px]"></div>
+          <div>
+            <span className="text-[#525C76] font-medium text-[14px] leading-[22.4px] capitalize">Installment</span>
+            <p className="text-[#0F1D40] text-[14px] leading-[22.4px] font-medium">{listing.installment ? "Yes" : "No"}</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="rounded-full bg-[#CACDD5] w-[50px] h-[50px]"></div>
+          <div>
+            <span className="text-[#525C76] font-medium text-[14px] leading-[22.4px] capitalize">Completion Year</span>
+            <p className="text-[#0F1D40] text-[14px] leading-[22.4px] font-medium">{listing.year || "N/A"}</p>
+          </div>
+        </div>
+      </div>
+  
+      {/* Description */}
+      <div className="my-14">
+        <h1 className="text-[#0F1D40] font-semibold text-[36px] leading-[54px]">Description</h1>
+        <p className="text-[#0F1D40] font-normal text-[18px] leading-[28.8px]">{listing.description}</p>
+        <hr className="my-8 border-t-2 border-[#EEEFF2]" />
+      </div>
+  
+      {/* Location */}
+      <div className="w-full border-t-2 py-14">
+        <h2 className="text-[#0F1D40] font-semibold text-[36px] leading-[54px]">Location</h2>
+        <PropertyMap
+          location={{
+            latitude: listing.latitude,
+            longitude: listing.longitude,
+          }}
+        />
+      </div>
     </div>
-  );
+  );  
 };
 
 export default Complex;
