008bcabf1e4ad400b9ef579656ae06eb91a86072
rasadov <raufasadov23@gmail.com>
2025-01-20 17:18:45 +0400

    fixes

--- src/components/sections/CardList.jsx
+++ src/components/sections/CardList.jsx
@@ -4,7 +4,7 @@ import SectionArea from "./SectionArea";
 export const CardList = ({ children, sectionName, seeAll = true }) => {
   return (
     <SectionArea sectionName={sectionName} seeAll={seeAll}>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5 gap-6 mx-auto">
         {children}
       </div>
     </SectionArea>
