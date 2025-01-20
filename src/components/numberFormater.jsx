89273980badc3284a9c503a8a356817739ff7649
rasadov <raufasadov23@gmail.com>
2025-01-20 16:26:18 +0400

    fixes

--- src/components/numberFormater.jsx
+++ src/components/numberFormater.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function formatNumber(number) {
     console.log("formatNumber called");
     return new Intl.NumberFormat('en-US', {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
     }).format(number);
   };
 
