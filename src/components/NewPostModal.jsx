79ad3edac92ff031afe1791addc973343735f736
FaridYusifzade <fyusifzadeh@gmail.com>
2025-02-02 10:42:33 +0400

    add

--- src/components/NewPostModal.jsx
+++ src/components/NewPostModal.jsx
@@ -271,7 +271,7 @@ const NewPostModal = ({ isOpen, onClose, complexes }) => {
       onClick={onClose}
     >
       <motion.div
-        className="w-full sm:w-[75%] md:w-[80%] h-[800px] sm:h-[990px] bg-white rounded-lg shadow-lg flex flex-col"
+        className="w-full sm:w-[55%] md:w-[60%] h-[800px] sm:h-[990px] bg-white rounded-lg shadow-lg flex flex-col"
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.3 }}
