76dd6ddbade0ab520098c6641839c94a45301eb4
rasadov <raufasadov23@gmail.com>
2025-02-03 11:40:18 +0400

    fixes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Rating from "../components/Rating";
 import CardList from "../components/sections/CardList";
@@ -16,6 +16,7 @@ import NewComplexModal from "../components/NewComplexModal.jsx";
 import { fetchPosts } from "../store/slices/agentPostSlice";
 import { NewComplex } from "../assets/icons/NewComplex.jsx";
 import { useNavigate } from "react-router-dom";
+import { AddUserImage } from "../assets/icons/AddUserImage";
 
 export const Profile = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -43,8 +44,8 @@ export const Profile = () => {
       })
         .then((res) => {
           if (res.status === 401) {
-            window.location.href = "/login";
-            navigate("/login");
+            // window.location.href = "/login";
+            // navigate("/login");
             localStorage.removeItem("user");
           }
           return res.json();
@@ -56,8 +57,8 @@ export const Profile = () => {
         })
         .catch((error) => {
           console.log(error);
-          window.location.href = "/login";
-          navigate("/login");
+          // window.location.href = "/login";
+          // navigate("/login");
           localStorage.removeItem("user");
         });
       const userResponse = await fetch(
@@ -130,84 +131,153 @@ export const Profile = () => {
     setIsComplexModalOpen(false);
   };
 
+  const fileInputRef = useRef(null);
+
+  // Trigger the hidden file input when the icon is clicked.
+  const handleIconClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  // Handle file selection and upload the image.
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+    if (!file) return; // No file selected
+
+    // Prepare the file to be sent as form data.
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const response = await fetch(
+        "https://api.flatty.ai/api/v1/user/profile/image",
+        {
+          method: "POST",
+          // Note: Do not set the "Content-Type" header when sending FormData.
+          body: formData,
+          credentials: "include",
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Upload failed: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      console.log("Upload successful:", data);
+      // Optionally, update your state or perform additional actions here.
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
+  };
+
   return (
     <div className="w-full py-3 mx-auto mt-8">
       <Breadcrumbs title="Apartment" />
       <div className="flex flex-col gap-6 mt-8 lg:flex-row">
-        {/* Agent Info */}
-        <div className="p-6 bg-white rounded-lg w-full lg:w-[578px] min-h-[272px]">
-          <div className="flex flex-col items-center lg:flex-row lg:items-start gap-4 mb-6">
-            <img
-              src={
-                user?.image_url
-                  ? user.image_url
-                  : "https://flattybucket.s3.us-east-1.amazonaws.com/uploads/user.jpg"
-              }
-              className="rounded-full w-[120px] h-[120px] object-cover"
-              alt="Agent"
-            />
-            <div className="w-full">
-              <div className="space-y-2">
-                <div className="flex items-center justify-between">
-                  <h2 className="text-lg font-semibold">
-                    {agent.user ? agent.user.name : ""}
-                  </h2>
-                </div>
-                <div className="flex items-center justify-start gap-4">
-                  <Rating rating="4" />
-                  <p className="text-sm text-[#525C76]">
-                    {agent.reviews ? agent.reviews.length + " Votes" : ""}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* Experience, Sales, Active Posts */}
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 bg-[#EEEFF2] p-4 rounded-md">
-            {["Experience", "Successful sales", "Active posts"].map(
-                (label, index) => (
-                  <div key={index} className="text-center">
-                    <span className="block text-sm text-gray-500">{label}</span>
-                    <p className="font-medium text-lg text-[#0F1D40]">
-                      {index === 0 ? agent.experience ? agent.experience + " years" : "Unknown" :
-                      index === 1 ? agent.sales ? agent.sales : "Unknown":
-                      index === 2 ? properties.length ? properties.length : "Unknown" : ""}
-                    </p>
-                  </div>
-                )
-              )}
-          </div>
-          {/* Buttons */}
-          <div className="flex flex-col lg:flex-row justify-center gap-4 mt-4">
-            <Button
-              className="w-full py-2 text-sm h-[45px] font-semibold text-white rounded-sm leading-[28px] text-[18px]"
-              variant="primary"
-              onClick={handleOpenModal}
-            >
-              <NewPost />
-              New Object
-            </Button>
-            <Button
-              className="w-full py-2 text-sm h-[45px] font-semibold text-[#8247E5] bg-white border border-[#8247E5] rounded-sm leading-[28px] text-[18px]"
-              variant="outline"
-              onClick={handleOpenComplexModal}
-            >
-              <NewComplex />
-              New Complex
-            </Button>
-          </div>
+  {/* Agent Info */}
+  <div className="p-6 bg-white rounded-lg w-full lg:w-[578px] min-h-[272px]">
+    <div className="flex flex-col items-center lg:flex-row lg:items-start gap-4 mb-6">
+      {/* Wrap the image in a relative container */}
+      <div className="relative">
+        <img
+          src={
+            user?.image_url
+              ? user.image_url
+              : "https://flattybucket.s3.us-east-1.amazonaws.com/uploads/user.jpg"
+          }
+          className="rounded-full w-[120px] h-[120px] object-cover"
+          style={{
+            minWidth: "120px",
+          }}
+          alt="Agent"
+        />
+        {/* Position the upload icon at the bottom-right corner */}
+        <div className="absolute bottom-0 right-0"
+        onClick={handleIconClick}
+        >
+          <AddUserImage />
         </div>
+        <input
+        type="file"
+        ref={fileInputRef}
+        className="hidden"
+        onChange={handleFileChange}
+        accept="image/*"
+      />
+      </div>
 
-        {/* Agent Image */}
-        <div className="w-full lg:w-[683px] h-[345px] relative">
-          <img
-            src={agent_back}
-            className="absolute w-full h-auto rounded-lg bottom-10"
-            alt="Agent"
-            loading="lazy"
-          />
+      <div className="w-full">
+        <div className="space-y-2">
+          <div className="flex items-center justify-between">
+            <h2 className="text-lg font-semibold">
+              {agent.user ? agent.user.name : ""}
+            </h2>
+          </div>
+          <div className="flex items-center justify-start gap-4">
+            <Rating rating="4" />
+            <p className="text-sm text-[#525C76]">
+              {agent.reviews ? agent.reviews.length + " Votes" : ""}
+            </p>
+          </div>
         </div>
       </div>
+    </div>
+    {/* Experience, Sales, Active Posts */}
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 bg-[#EEEFF2] p-4 rounded-md">
+      {["Experience", "Successful sales", "Active posts"].map((label, index) => (
+        <div key={index} className="text-center">
+          <span className="block text-sm text-gray-500">{label}</span>
+          <p className="font-medium text-lg text-[#0F1D40]">
+            {index === 0
+              ? agent.experience
+                ? agent.experience + " years"
+                : "Unknown"
+              : index === 1
+              ? agent.sales
+                ? agent.sales
+                : "Unknown"
+              : index === 2
+              ? properties.length
+                ? properties.length
+                : "Unknown"
+              : ""}
+          </p>
+        </div>
+      ))}
+    </div>
+    {/* Buttons */}
+    <div className="flex flex-col lg:flex-row justify-center gap-4 mt-4">
+      <Button
+        className="w-full py-2 text-sm h-[45px] font-semibold text-white rounded-sm leading-[28px] text-[18px]"
+        variant="primary"
+        onClick={handleOpenModal}
+      >
+        <NewPost />
+        New Object
+      </Button>
+      <Button
+        className="w-full py-2 text-sm h-[45px] font-semibold text-[#8247E5] bg-white border border-[#8247E5] rounded-sm leading-[28px] text-[18px]"
+        variant="outline"
+        onClick={handleOpenComplexModal}
+      >
+        <NewComplex />
+        New Complex
+      </Button>
+    </div>
+  </div>
+
+  {/* Agent Image */}
+  <div className="w-full lg:w-[683px] h-[345px] relative">
+    <img
+      src={agent_back}
+      className="absolute w-full h-auto rounded-lg bottom-10"
+      alt="Agent"
+      loading="lazy"
+    />
+  </div>
+</div>
 
       {/* Active Posts Section */}
       <CardList sectionName="My posts" seeAll={false}>
